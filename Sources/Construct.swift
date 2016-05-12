22e9dd98642a2977922406a87933ea30e821f681
antonmes <antonmes@users.noreply.github.com>
2016-05-13 00:48:40 +0300

    Fix warnings

--- Sources/Construct.swift
+++ Sources/Construct.swift
@@ -4,7 +4,7 @@ public protocol Initializable : class {
 }
 
 /// Create a class or struct with a constructor method. Return a value of `property.type` for each property. Classes must conform to `Initializable`.
-public func construct<T>(_ type: T.Type = T.self, constructor: Property.Description throws -> Any) throws -> T {
+public func construct<T>(_ type: T.Type = T.self, constructor: (Property.Description) throws -> Any) throws -> T {
     guard Metadata(type: T.self).isStructOrClass else { throw Error.notStructOrClass(type: T.self) }
     if Metadata(type: T.self)?.kind == .struct {
         return try constructValueType(constructor)
@@ -15,7 +15,7 @@ public func construct<T>(_ type: T.Type = T.self, constructor: Property.Descript
     }
 }
 
-private func constructValueType<T>(_ constructor: Property.Description throws -> Any) throws -> T {
+private func constructValueType<T>(_ constructor: (Property.Description) throws -> Any) throws -> T {
     guard Metadata(type: T.self)?.kind == .struct else { throw Error.notStructOrClass(type: T.self) }
     let pointer = UnsafeMutablePointer<T>(allocatingCapacity: 1)
     defer { pointer.deallocateCapacity(1) }
@@ -25,7 +25,7 @@ private func constructValueType<T>(_ constructor: Property.Description throws ->
     return pointer.pointee
 }
 
-private func constructReferenceType<T>(_ value: T, constructor: Property.Description throws -> Any) throws -> T {
+private func constructReferenceType<T>(_ value: T, constructor: (Property.Description) throws -> Any) throws -> T {
     var copy = value
     var storage = mutableStorageForInstance(&copy)
     var values = [Any]()
@@ -33,7 +33,7 @@ private func constructReferenceType<T>(_ value: T, constructor: Property.Descrip
     return copy
 }
 
-private func constructType(storage: inout UnsafeMutablePointer<Int>, values: inout [Any], properties: [Property.Description], constructor: Property.Description throws -> Any) throws {
+private func constructType(storage: inout UnsafeMutablePointer<Int>, values: inout [Any], properties: [Property.Description], constructor: (Property.Description) throws -> Any) throws {
     for property in properties {
         var value = try constructor(property)
         guard Reflection.value(value, is: property.type) else { throw Error.valueIsNotType(value: value, type: property.type) }
@@ -47,7 +47,7 @@ public func construct<T>(_ type: T.Type = T.self, dictionary: [String: Any]) thr
     return try construct(constructor: constructorForDictionary(dictionary))
 }
 
-private func constructorForDictionary(_ dictionary: [String: Any]) -> Property.Description throws -> Any {
+private func constructorForDictionary(_ dictionary: [String: Any]) -> (Property.Description) throws -> Any {
     return { property in
         if let value = dictionary[property.key] {
             return value
