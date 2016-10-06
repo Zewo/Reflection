ca834e4a03df229e9b3bd476c44f2d5b5207ff2c
Ivan Smolin <ivan.smolin@touchin.ru>
2016-10-06 16:47:49 +0300

    add Xcode project with targets and tests for iOS, watchOS, tvOS, macOS.

---
+++ Sources/Reflection.h
@@ -0,0 +1,19 @@
+//
+//  Reflection.h
+//  Reflection
+//
+//  Created by Ivan Smolin on 06/10/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Reflection.
+FOUNDATION_EXPORT double ReflectionVersionNumber;
+
+//! Project version string for Reflection.
+FOUNDATION_EXPORT const unsigned char ReflectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reflection/PublicHeader.h>
+
+
