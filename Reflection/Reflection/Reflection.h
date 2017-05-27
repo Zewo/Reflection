9a59686b5a7c31e94c49f2d1704b5f47b1c4b96e
Tony Henderson <info@vizifit.com>
2017-05-27 15:24:56 -0700

    Initial Commit

---
+++ Reflection/Reflection/Reflection.h
@@ -0,0 +1,19 @@
+//
+//  Reflection.h
+//  Reflection
+//
+//  Created by Tony Henderson on 5/27/17.
+//  Copyright © 2017 Tony Henderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reflection.
+FOUNDATION_EXPORT double ReflectionVersionNumber;
+
+//! Project version string for Reflection.
+FOUNDATION_EXPORT const unsigned char ReflectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reflection/PublicHeader.h>
+
+
